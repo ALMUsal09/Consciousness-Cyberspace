3ac7f0463df2182806c5c9eab37586526f721cd9
ALMUsal09 <123786453+ALMUsal09@users.noreply.github.com>
2023-01-28 09:49:02 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,276 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Cyberspace Conciousness: Microblogging </title>
+
+<link rel="stylesheet" href="style.css">
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
+
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+</head>
+<body>
+	<!----hero Section start---->
+ 
+	<div class="hero">
+	
+		<nav>
+		
+			<h2 class="logo"><span><img src="images/logo.png"class="logo"></span></h2>
+			<div class="navbar">
+			<ul>
+				<li><a href="#">Home</a></li>
+				<li><a href="#">About Us</a></li>
+				<li><a href="#">Forms</a></li>
+				<li><a href="#">Contact Us</a></li>
+			</ul>
+				  <img src="images/menu.png" class= "menu-icon">
+
+		</nav>
+
+		<div class="content">
+			
+			<h1>Conciousness Cyberspace</h1>
+			<h3>This website contains different forms of Cybercrimes</h3>
+			<div class="newslatter">
+				<form>
+					<input type="email" name="email" id="mail" placeholder="Search">
+					<input type="submit" name="submit" value="Explore">
+				</form>
+			</div>
+		</div>
+	</div>
+
+	<!----About section start---->
+		<section class="about">
+			<div class="main">
+				<img src="images/aboutus.png" style="width:500px;height:550px; border-radius: 5%;"}>
+						<div class="about-text">
+				<h2>About Us</h2>
+				<h5>Researchers<span> From 12 ICT 2</span></h5>
+<p>We, the researchers aim to spread awareness regarding cyber crime to the students and teachers of Arellano University Jose Abad Santos. Cyberspace Microblogging plays a big role in our study because our study takes place in a  virtual setup. Since we want to spread awareness, regarding cyber crime, we came up to this word consciousness with the word "con" being highlighted. It is to draw attention to what we would like them to be aware of. The term "con" is another term for someone who engages in unethical behavior or deceives others; it is also used to define scams and fraud.</p>
+				<button type="button">Okay</button>
+			</div>
+		</div>
+	</section>
+
+	<!-----service section start----------->
+	<div class="service">
+		<div class="title">
+			<h2>Cyber Crimes</h2>
+		</div>
+
+		<div class="box">
+			<div class="card">
+				<i class="fas fa-user-circle"></i>
+				<h5>Phishing</h5>
+				<div class="pra">
+					<p>Phishing is when attackers send malicious emails designed to trick people into falling for a scam.</p>
+
+					<p style="text-align: center;">
+						<a class="button" href="#">Read More</a>
+					</p>
+				</div>
+			</div>
+
+			<div class="card">
+				<i class="fas fa-hand-rock"></i>
+				<h5>Cyber Bullying</h5>
+				<div class="pra">
+					<p>Cyber Bullying is a common cybercrime that mostly happens, especially among teenagers. Moreover, it works by using electrical means.</p>
+
+					<p style="text-align: center;">
+						<a class="button" href="#">Read More</a>
+					</p>
+				</div>
+			</div>
+
+<div class="card">
+				<i class="fas fa-eye"></i>
+				<h5>Cyber Stalking</h5>
+				<div class="pra">
+					<p>Cyberstalking is a crime committed when someone uses the internet and other technologies to harass or stalk another person online.</p>
+
+					<p style="text-align: center;">
+						<a class="button" href="#">Read More</a>
+					</p>
+				</div>
+			</div>
+
+			<div class="card">
+				<i class="fab fa-creative-commons-pd"></i>
+				<h5>Copyright Infringement</h5> 
+				<div class="pra">
+					<p>Copyright infringement is the use or production of copyright-protected material without the permission of the copyright holder.</p>
+
+					<p style="text-align: center;">
+						<a class="button" href="#">Read More</a>
+					</p>
+				</section>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	
+		<section class="about1">
+			<div class="box">
+			<div class="card">
+				<i class="fa fa-users"></i>
+				<h5>Cyber Harassment</h5>
+				<div class="pra">
+					<p>Cyber Wellness is about the proactive and positive use of the internet, social and mobile applications.</p>
+
+					<p style="text-align: center;">
+						<a class="button" href="#">Read More</a>
+					</p>
+				</div>
+			</div>
+			<div class="card">
+				<i class="fa fa-credit-card"></i>
+				<h5>Credit Card Fraud</h5>
+				<div class="pra">
+					<p>Credit card fraud is an inclusive term for fraud committed using a payment card, such as a credit card or debit card.</p>
+
+					<p style="text-align: center;">
+						<a class="button" href="#">Read More</a>
+					</p>
+				</div>
+			</div>
+			<div class="card">
+				<i class="fa fa-object-group"></i>
+				<h5>Software Piracy</h5>
+				<div class="pra">
+					<p>The use or distribution of copyrighted computer software in violation of the copyright laws or applicable license restrictions.</p>
+
+					<p style="text-align: center;">
+						<a class="button" href="#">Read More</a>
+					</p>
+				</div>
+			</div><div class="card">
+				<i class="fa fa-envelope"></i>
+				<h5>Spamming</h5>
+				<div class="pra">
+					<p>Spam is any or all unwanted emails, texts, or marketing ads you receive or send.</p>
+
+					<p style="text-align: center;">
+						<a class="button" href="#">Read More</a>
+					</p>
+				</section>
+				</div>
+			</div>
+			</div>
+
+			<section class="about2">
+			<div class="box">
+			<div class="card">
+				<i class="fa fa-child"></i>
+				<h5>Sextortion</h5>
+				<div class="pra">
+					<p>Sextortion is the criminal act of blackmailing someone to force them into doing sexual favors or acts.</p>
+
+					<p style="text-align: center;">
+						<a class="button" href="#">Read More</a>
+					</p>
+				</div>
+			</div>
+
+			<div class="card">
+				<i class="fa fa-exclamation-triangle"></i>
+				<h5>Malware</h5>
+				<div class="pra">
+					<p>Malware is a collective term for viruses, ransomware, spyware, Trojans, and any other type of code or software created with malicious intent.</p>
+
+					<p style="text-align: center;">
+						<a class="button" href="#">Read More</a>
+					</p>
+				</div>
+			</div>
+
+			<div class="card">
+				<i class="fa fa-newspaper"></i>
+				<h5>Online Defamation</h5>
+				<div class="pra">
+					<p>They are posts intended to suggest something, nudes (or even fakes) have been uploaded, and individuals have allegedly been exposed.</p>
+
+					<p style="text-align: center;">
+						<a class="button" href="#">Read More</a>
+					</p>
+				</div>
+			</div>
+
+			<div class="card">
+				<i class="fa fa-user-secret"></i>
+				<h5>Account Hacking</h5>
+				<div class="pra">
+					<p>A sort of cyberattack in which a hacker gives up access of the victim's account on a computer or an online service.</p>
+
+					<p style="text-align: center;">
+						<a class="button" href="#">Read More</a>
+					</p>
+					</section>
+
+				</div>
+			</div>
+
+			<section class="about3">
+			<div class="box">
+			<div class="card">
+				<i class="fas fa-american-sign-language-interpreting"></i>
+				<h5>Cyber Grooming</h5>
+				<div class="pra">
+					<p>Cyber grooming is the act of developing a trustworthy relationship with a minor online in order to commit a sexual misconduct.</p>
+
+					<p style="text-align: center;">
+						<a class="button" href="#">Read More</a>
+					</p>
+				</div>
+			</div>
+
+			<div class="card">
+				<i class="fas fa-coins"></i>
+				<h5>Scamming</h5>
+				<div class="pra">
+					<p>Is a sort of cybercrime that involves false information in order to defraud people of their hard-earned cash, assets, and inheritance.</p>
+
+					<p style="text-align: center;">
+						<a class="button" href="#">Read More</a>
+					</p>
+				</div>
+			</div>
+
+			<div class="card">
+				<i class="fas fa-copy"></i>
+				<h5>Identity Theft</h5>
+				<div class="pra">
+					<p>Identity theft is when someone uses your personal or financial information without your permission.</p>
+
+					<p style="text-align: center;">
+						<a class="button" href="#">Read More</a>
+					</p>
+				</div>
+			</div>
+
+		</section>
+
+	<!------Contact Me------>
+	<div class="contact-me">
+		<p>What is Cybercrime?</p>
+		<a class="button-two" href="#">Cybercrime is any illegal activities that occurs with the help of internet.</a>
+	</div>
+
+	<!------footer start--------->
+	<footer>
+		<p>Group Six of 12 ICT 2 in AUJAS</p>
+		<p>If you think that our website is helpful, let us know by contacting us to the socials that we provided below:</p>
+		<div class="social">
+			<a href="#"><i class="fab fa-facebook-f"></i></a>
+			<a href="#"><i class="fab fa-instagram"></i></a>
+			<a href="#"><i class="fab fa-google"></i></a>
+		</div>
+		<p class="end">CopyRight By Tahmid Ahmed</p>
+	</footer>
+</body>
+</html>
+
